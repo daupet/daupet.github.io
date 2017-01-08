0a56ff96fa545f0fd0db71c408ffeb6f083a661b
daupet <ptjpwm992@gmail.com>
2017-01-08 17:26:59 +0900

    Refactor and add comments

--- simple-pendulum/simple-pendulum.js
+++ simple-pendulum/simple-pendulum.js
@@ -1,6 +1,6 @@
 var canvas;
 
-var ppm = 10; // the number of pixels per meter
+var ppm = 10; // pixel per meter
 var dT = 0.001; // sampling interval [sec]
 var g = 9.8*ppm*ppm; // gravity [px/sec/sec]
 var l = 100; // length of pendulum
@@ -17,29 +17,29 @@ function setup()
     canvas.parent("canvas_container");
     canvas.style("background-color", "white");
 
+    // initialize variables
     p0 = createVector(width/2, height/2);
     p  = createVector(0, 0);
-    th = 1.57079632679489661923;
-    oo = 0.0;
-    poo = 0.0;
-    pacc = 0.0;
-    state = [th, oo];
-    p.x = l*sin(th);
-    p.y = l*cos(th);
+    state = [1.57079632679489661923, 0.0];
+    p.x = l*sin(state[0]);
+    p.y = l*cos(state[0]);
     p.add(p0);
 }
 
 
 function draw()
 {
+    // the number of time evolution at this frame
     N = int(1/frameRate() / dT);
     if (isFinite(N) == false) N = 0;
 
+    // calculating time evolution N times
     for (var i=0; i<N; i++)
     {
         state = runge_kutta(state, F, dT);
     }
 
+    // transforming generalized coordinate to Cartesian coordinate
     p.x = l*sin(state[0]);
     p.y = l*cos(state[0]);
     p.add(p0);
@@ -53,6 +53,14 @@ function draw()
 }
 
 
+/**
+ * Calculating time evolution by using Runge-Kutta method.
+ *
+ * @param {Array} x state vector at a time.
+ * @param {function} f function calculating the time differentiation of a state vector.
+ * @param {number} dt sampling interval.
+ * @return {Array} state vector at the next sampling.
+ */
 function runge_kutta(x, f, dt)
 {
     k1 = f(x, 0);
@@ -79,6 +87,13 @@ function runge_kutta(x, f, dt)
 }
 
 
+/**
+ * Calculating time differentiation of a state vector for simple pendulum.
+ *
+ * @param {Array} x state vector consists of [theta, omega].
+ * @param {number} dt sampling interval (not used).
+ * @return {Array} time differentiation of x: [omega, accel].
+ */
 function F(x, dt)
 {
     theta = x[0];
