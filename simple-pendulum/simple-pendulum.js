db23efcfabfb66299d43f2c523217053b2305289
daupet <ptjpwm992@gmail.com>
2017-01-08 21:19:48 +0900

    Add function of grabbing the pendulum

--- simple-pendulum/simple-pendulum.js
+++ simple-pendulum/simple-pendulum.js
@@ -9,13 +9,24 @@ var p0; // position of origin
 var p; // [x, y]: position in Cartesian coordinate system
 var state; // [theta, omega]: state vector in generalized coordinate system
 
+var p_grab; // position of grabbing [px]
+var grip = 1000; // strength of grip [1/sec/sec]
+var fric = 2*Math.sqrt(50*l*grip); // friction coefficient [px/sec]
+var is_grabbing; // whether grabbing the pendulum or not
+
 
 function setup()
 {
     // create canvas
     canvas = createCanvas(480, 480);
+    canvas.id("p5_canvas");
     canvas.parent("canvas_container");
     canvas.style("background-color", "white");
+
+    // disable scrolling while the canvas is being touched
+    var elem_canvas = document.getElementById("p5_canvas");
+    elem_canvas.addEventListener("touchstart", function(e){ e.preventDefault(); }, false);
+    elem_canvas.addEventListener("touchmove", function(e){ e.preventDefault(); }, false);
 
     // initialize variables
     p0 = createVector(width/2, height/2);
@@ -29,11 +40,19 @@ function setup()
             g.x = -e.accelerationIncludingGravity.x * ppm;
             g.y = e.accelerationIncludingGravity.y * ppm;
         }, false);
+
+    p_grab = createVector(0, 0);
+    is_grabbing = false;
 }
 
 
 function draw()
 {
+    if (is_grabbing)
+    {
+        calculate_grab();
+    }
+
     // the number of time evolution at this frame
     N = int(1/frameRate() / dT);
     if (isFinite(N) == false) N = 0;
@@ -57,13 +76,19 @@ function draw()
 
     // drawing
     clear();
+
+    // displaying acceleration as text
     noStroke();
     textSize(24);
     text("accel X: " + g.x/ppm + "\n" + "accel Y: " + g.y/ppm, 10, 30);
+
+    // drawing pendulum
     stroke(0, 0, 0);
     fill(0, 0, 0);
     line(p0.x, p0.y, p.x, p.y);
     ellipse(p.x, p.y, 30);
+
+    //displaying acceleration as line
     stroke(0, 128, 128);
     line(p.x, p.y, p.x+g.x/ppm*10, p.y+g.y/ppm*10);
 }
@@ -116,8 +141,41 @@ function F(x, dt)
     omega = x[1];
 
     var g_th = g.x*cos(theta) - g.y*sin(theta)
+    var grab_th = 0;
+    if (is_grabbing)
+    {
+        var grab_th = grip * (p_grab.x*cos(theta) - p_grab.y*sin(theta));
+        grab_th -= fric*omega;
+    }
 
-    var accel = (g_th) / l;
+    var accel = (g_th + grab_th) / l;
 
     return [omega, accel];
+}
+
+
+function touchStarted()
+{
+    is_grabbing = true;
+}
+
+
+function touchEnded()
+{
+    is_grabbing = false;
+}
+
+
+function calculate_grab()
+{
+    // get position of mouse or touch
+    if (touches.length > 0)
+    {
+        p_grab = createVector(touches[0].x, touches[0].y);
+    }
+    else
+    {
+        p_grab = createVector(mouseX, mouseY);
+    }
+    p_grab.sub(p0);
 }
